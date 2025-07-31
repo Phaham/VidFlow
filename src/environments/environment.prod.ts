27e73f196233204267ab318a5c045995f283ed53
Phaham Khan <72141945+Phaham@users.noreply.github.com>
2025-07-31 12:19:16 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,16 @@ export const environment = {
   production: true,
   firebase: {
     // Replace these with your own Firebase project config
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'your-project-id.firebaseapp.com',
-    projectId: 'your-project-id',
-    storageBucket: 'your-project-id.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    // apiKey: 'YOUR_API_KEY',
+    // authDomain: 'your-project-id.firebaseapp.com',
+    // projectId: 'your-project-id',
+    // storageBucket: 'your-project-id.appspot.com',
+    // messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    // appId: 'YOUR_APP_ID',
+    apiKey: 'AIzaSyD6RLsNn8FpjHvLkG4EX19PKuKUDKWIt9Q',
+    authDomain: 'clips-90c75.firebaseapp.com',
+    projectId: 'clips-90c75',
+    storageBucket: 'clips-90c75.appspot.com',
+    appId: '1:1068166561534:web:719afb3cc06f8c5719afaf',
   },
 };
